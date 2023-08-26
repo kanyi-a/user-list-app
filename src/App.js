3c81f0355f737d7ad66d0bffcd979197e5ba76ba
kanyi-a <angela.kanyi@student.moringaschool.com>
2023-08-27 01:01:05 +0300

    adding POST request

--- src/App.js
+++ src/App.js
@@ -23,6 +23,15 @@ function App() {
    const handleSubmit = async e => {
     e.preventDefault();
 
+     try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newUser),
+      });
+     
   return (
     <div className="App">
       <h1>User List</h1>
